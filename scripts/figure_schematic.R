5092cdd69ee5c9bffdb90bcc6b3ac480c6582591
parksw3 <parksw3@mcmaster.ca>
2022-11-05 02:07:53 -0400

    tweaking schematic

--- scripts/figure_schematic.R
+++ scripts/figure_schematic.R
@@ -47,25 +47,23 @@ generate_base_plot <- function(future = FALSE) {
   return(base_plot)
 }
 
+add_arrow <- function(plot, y, ends, angle=30, lty=1, length=0.1, ...) {
+  plot <- plot + 
+    geom_segment(
+      aes(..., y = y, yend = y),
+      arrow = arrow(length = unit(length, "inches"), ends = ends, angle=angle), lwd = 1, lty=lty
+    )
+  return(plot)
+}
 
-truncation <- generate_base_plot(future = TRUE) +
-  geom_segment(
-    aes(0 + epsilon, 0, xend = 1 - epsilon, yend = 0),
-    arrow = arrow(length = unit(0.2, "inches")), lwd = 1
-  ) +
-  geom_segment(
-    aes(0 + epsilon, 0.1, xend = 4 - epsilon, yend = 0.1),
-    arrow = arrow(length = unit(0.2, "inches")), lwd = 1, lty = 2
-  ) +
-  geom_segment(
-    aes(1 + epsilon, 0.2, xend = 2 - epsilon, yend = 0.2),
-    arrow = arrow(length = unit(0.2, "inches")), lwd = 1
-  ) +
-  geom_segment(
-    aes(4 + epsilon, 0.3, xend = 5 - epsilon, yend = 0.3),
-    arrow = arrow(length = unit(0.2, "inches")), lwd = 1, lty = 2
-  ) +
+truncation <- generate_base_plot(future = TRUE) |>
+  add_arrow(x=0.1, y=0, xend=0.9, ends="last") |>
+  add_arrow(x=1.1, y=0.2, xend=1.9, ends="last") |>
+  add_arrow(x=0.1, y=0.1, xend=3.9, ends="last", lty=2, length=0) |>
+  add_arrow(x=4.1, y=0.3, xend=4.9, ends="last", lty=2, length=0) +
   geom_segment(aes(3, -0.05, xend = 3, yend = 0.35), lty = 1, col = "gray50") +
+  annotate("text", x = 0.5, y = 0.01, label = "Observed delay", vjust = -0.5) +
+  annotate("text", x = 2, y = 0.11, label = "Unobserved delay", vjust = -0.5) + ## not-yet-observed? unobserved just sounds like a bad sample
   annotate("text", x = 3, y = 0.35, label = "Observation time", vjust = -0.5) +
   annotate("text", x = 4.5, y = -0.05, label = "Future events", vjust = -1) +
   geom_rect(
@@ -73,22 +71,17 @@ truncation <- generate_base_plot(future = TRUE) +
     alpha = 0.2
   )
 
-add_arrow <- function(plot, y, ends, ...) {
-  plot <- plot + 
-    geom_segment(
-      aes(..., y = y, yend = y),
-      arrow = arrow(length = unit(0.1, "inches"), ends = ends), lwd = 1
-    )
-  return(plot)
-}
 censoring <- generate_base_plot(future = FALSE) |>
-  add_arrow(-0.45, y = 0, xend = -.1, ends = "first") |>
-  add_arrow(0.1, y = 0, xend = 0.45, ends = "last") |>
-  add_arrow(0.55, y = 0, xend = 0.9, ends = "first") |>
-  add_arrow(1.1, y = 0, xend = 1.45, ends = "last") |>
-  add_arrow(0.1, y = 0.1, xend = 2.45, ends = "last") |>
-  add_arrow(3.55, y = 0.1, xend = 3.9, ends = "first") |>
-  add_arrow(4.1, y = 0.1, xend = 4.45, ends = "last") +
+  add_arrow(x=-0.45, y = 0, xend = -.1, ends = "first", angle=90) |>
+  add_arrow(x=0.1, y = 0, xend = 0.45, ends = "last", angle=90) |>
+  add_arrow(x=0.55, y = 0, xend = 0.9, ends = "first", angle=90) |>
+  add_arrow(x=1.1, y = 0, xend = 1.45, ends = "last", angle=90) |>
+  # add_arrow(x=-0.45, y = 0.1, xend = -.1, ends = "first", angle=90, lty=2) |>
+  # add_arrow(x=0.1, y = 0.1, xend = 3.45, ends = "last", angle=90) |>
+  add_arrow(x=3.55, y = 0.1, xend = 3.9, ends = "first", angle=90) |>
+  add_arrow(x=4.1, y = 0.1, xend = 4.45, ends = "last", angle=90) |>
+  add_arrow(x=1.1, y = 0.2, xend = 1.9, ends = "last", angle=30) |>
+  add_arrow(x=4.1, y = 0.3, xend = 4.9, ends = "last", angle=30) +
   theme(
     panel.grid = element_blank(),
     panel.border = element_blank(),
